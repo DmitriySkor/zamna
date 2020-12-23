968bba380accf0e463ce4b08fa8f7918584f51ef
ValentinGor <v.gorbatenko@mail.ru>
2020-12-23 13:15:10 +0200

    Added mobile menu.

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,30 @@
+// Мобильное меню
+document.getElementById("mobile_menu_open_btn").onclick = function (e) {
+    e.preventDefault();
+    document.querySelector("body").classList.toggle("fix");
+    document.getElementById("mobile_menu_open_btn").classList.toggle("active");
+    document.getElementById("mobile_menu_list_ulli").classList.toggle("active");
+}
+
 // Инициализация owlCarousel
 $(document).ready(function () {
     $('.main_carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        dots:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                loop:true
+        loop: true,
+        margin: 10,
+        dots: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                loop: true
             },
-            600:{
-                items:1,
-                loop:true
+            600: {
+                items: 1,
+                loop: true
             },
-            1000:{
-                items:1,
-                loop:false
+            1000: {
+                items: 1,
+                loop: false
             }
         }
     });
